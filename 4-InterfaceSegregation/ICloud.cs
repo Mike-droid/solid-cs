a237cc5c03f4b564656f4299d43bb546a188b092
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2023-05-21 14:02:24 -0500

    I principle

---
+++ 4-InterfaceSegregation/ICloud.cs
@@ -0,0 +1,10 @@
+namespace InterfaceSegregation
+{
+  public class Cloud
+  {
+    public void CreateCloud()
+    {
+      System.Console.WriteLine("I'm creating the Cloud Infrastructure");
+    }
+  }
+}
